71b503862d18e4a83fe9c7bcea3fa81e304c7dc0
souravbiswas19 <souravbiswas19april@gmail.com>
2024-02-24 17:29:25 +0530

    File Updated

--- google_drive_reader.py
+++ google_drive_reader.py
@@ -1,2 +1,10 @@
 from llama_index.readers.google import GoogleDriveReader
 loader = GoogleDriveReader()
+
+def load_data(folder_id: str):
+    docs = loader.load_data(folder_id=folder_id)
+    # for doc in docs:
+    #     doc.id_ = doc.metadata["file_name"]
+    return docs
+
+docs = load_data(folder_id="1cxqK_bHH5qdUrWSP8nx1s6_EwtFhcFRa")
