369f45bbf10ae9ceafc0d54d22175beaffb66d7c
souravbiswas19 <souravbiswas19april@gmail.com>
2024-02-26 10:10:27 +0530

    Front end main.py added

---
+++ main.py
@@ -0,0 +1,41 @@
+"""This a python file for frontend of the Intelligent Document Finder using LlamaIndex"""
+import time
+import streamlit as st
+from streamlit_extras.add_vertical_space import add_vertical_space
+
+with st.sidebar:
+    body = "🤖Intelligent Document Finder📃🔍"
+    st.header(body)
+    st.markdown('''
+    ## About
+    This app is an LLM-powered Intelligent Document Finder:
+    - [LlamaIndex](<https://www.llamaindex.ai/>)
+    - [GoogleGemini](<https://gemini.google.com/>)
+    - [HuggingFace](<https://huggingface.co/>) LLM model
+    
+    ''')
+    # st.button("Reload from Google Drive", type="primary")
+    # if st.button('Say hello'):
+    #     st.write('Why hello there')
+    # else:
+    #     st.write('Goodbye')
+    add_vertical_space(5)
+    add_vertical_space(5)
+    st.write('Made with ❤️ by [Sourav Biswas](<https://github.com/souravbiswas19>)')
+st.header("🤖Intelligent Document Finder📃🔍")
+# st.header("Your Query Here👇🏽")
+# title = st.text_input('')
+# print(title)
+prompt = st.chat_input("Say something")
+if prompt:
+    """
+    Reload the google drive after every prompt
+    1. Will increase the processing time
+    2. Will check if there is any new documents that has been uploaded in the folder
+    """
+
+    """
+    Query to be passed to the function to fetch the answer to the query
+    """
+    st.write(f"User has sent the following prompt: {prompt}")
+
