f9cb001ca2769d9a49ed601aa8ffc51e05b8f4e2
souravbiswas19 <souravbiswas19april@gmail.com>
2024-02-29 16:34:09 +0530

    Automation for New File Update done

--- google_drive_reader.py
+++ google_drive_reader.py
@@ -1,21 +1,35 @@
 """This python module implements the access of files from the Google Drive Folder"""
 from config import config
+from instance_flag import old_file_id, new_file_id
 from llama_index.readers.google import GoogleDriveReader
+
+# Intialization of the Google Drive Loader
+loader = GoogleDriveReader()
+
+def load_data(folder_id: str):
+    doc = loader.load_data(folder_id=folder_id)
+    return doc
+
+def load_new_data(old_docs, all_docs):
+    for i in old_docs:
+            old_file_id.add(i.id_)
+    for i in all_docs:
+        new_file_id.add(i.id_)
+    unique_file_id = new_file_id.symmetric_difference(old_file_id)
+    new_docs=[]
+    for i in unique_file_id:
+        for j in all_docs:
+            if i == j.id_:
+                new_docs.append(j)
+    return new_docs
+
 #Handling exception if folder cannot be accessed
-try:
-    # Intialization of the Google Drive Loader
-    loader = GoogleDriveReader()
-
-    def load_data(folder_id: str):
-        """Function to load data from the 
-        Google Drive Folder using the specified Folder ID"""
-        doc = loader.load_data(folder_id=folder_id)
-        # the list of documents is being returned
-        return doc
-
+try:    
     # load_data function is being called and list of documents is being stored
+    print("Data Loading Started...")
     docs = load_data(folder_id=config['FOLDER_ID'])
-    print("Data successfully loaded from folder")# Printint the sucess message on success retrieval of data from folder
+    print("Loading Done")    
+    # Printint the sucess message on success retrieval of data from folder
 except Exception as e: # If folder cannot be accessed exception is raised
     print(f"Error occurred while loading data from Google Drive: {e}") # Prinitng the exception
     docs = None
